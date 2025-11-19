9c6420f2ce8224e7ddc26951ee0730aef44a66a6
Fabio Melo <fabio.melo@hotmail.com>
2025-11-19 17:00:23 -0300

    batch linux

--- preparar.sh
+++ preparar.sh
@@ -4,10 +4,9 @@ sudo yum update -y
 sudo yum install git -y
 sudo yum install -y dotnet-sdk-8.0
 sudo yum install nginx -y
-sudo rm -rf ./Marketing/
 sudo rm -rf /var/incidenciaApi/
 sudo rm -rf /var/incidencia/
-cd Marketing/Marketing.Mvc
+cd Marketing.Mvc
 sudo dotnet publish -c Release -r linux-x64 ./Marketing.Mvc.csproj -o /var/incidencia/
 sudo chown -R ec2-user /var/incidencia
 sudo chmod 770 /var/incidencia/*
