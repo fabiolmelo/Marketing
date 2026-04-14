cc06a40f4a127ebe17248b8298239b9b3ecd668b
Fabio Melo <fabio.melo@hotmail.com>
2026-04-13 21:28:02 -0300

    ajuste grafico

--- Marketing.Mvc/Program.cs
+++ Marketing.Mvc/Program.cs
@@ -167,18 +167,18 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-// Security Headers
-app.Use(async (context, next) =>
-{
-    context.Response.Headers.TryAdd("X-Content-Type-Options", "nosniff");
-    context.Response.Headers.TryAdd("X-Frame-Options", "DENY");
-    context.Response.Headers.TryAdd("Referrer-Policy", "no-referrer");
-    context.Response.Headers.TryAdd("X-XSS-Protection", "1; mode=block");
-    context.Response.Headers.TryAdd("Content-Security-Policy",
-        "default-src 'self'; object-src 'none'; frame-ancestors 'none';");
+// // Security Headers
+// app.Use(async (context, next) =>
+// {
+//     context.Response.Headers.TryAdd("X-Content-Type-Options", "nosniff");
+//     context.Response.Headers.TryAdd("X-Frame-Options", "DENY");
+//     context.Response.Headers.TryAdd("Referrer-Policy", "no-referrer");
+//     context.Response.Headers.TryAdd("X-XSS-Protection", "1; mode=block");
+//     context.Response.Headers.TryAdd("Content-Security-Policy",
+//         "default-src 'self'; object-src 'none'; frame-ancestors 'none';");
 
-    await next();
-});
+//     await next();
+// });
 
 #endregion
 
